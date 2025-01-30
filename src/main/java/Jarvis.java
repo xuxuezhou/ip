4d2eea606716e875e27285bfcc2e54c0d233cfbe
xuxuezhou <xxz020928@gmail.com>
2025-01-30 16:18:45 +0800

    Completed Level 0: Rename, Greet, Exit

---
+++ src/main/java/Jarvis.java
@@ -0,0 +1,18 @@
+public class Jarvis {
+    public static void main(String[] args) {
+        String logo = "      ____.                  .__        \n"
+                + "     |    |____ __________  _|__| ______\n"
+                + "     |    \\__  \\_  __ \\  \\/ /  |/  ___/\n"
+                + " /\\__|    |/ __ \\|  | \\/\\   /|  |\\___ \\ \n"
+                + " \\________(____  /__|    \\_/ |__/____  >\n"
+                + "               \\/                    \\/ \n";
+
+        System.out.println(logo);
+        System.out.println("_________________________________________");
+        System.out.println("Hello! I'm Jarvis");
+        System.out.println("What can I do for you?");
+        System.out.println("_________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("_________________________________________");
+    }
+}
